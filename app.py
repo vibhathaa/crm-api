cb7fb226802837d2e453b5322921e75dc9a58796
e5 <algamavibhath@gmail.com>
2022-11-24 05:37:31 +0530

    Remove debug=true from app start configurations

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = config.connexion_app
 app.add_api(config.basedir / "openapi/swagger.yml")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000)
